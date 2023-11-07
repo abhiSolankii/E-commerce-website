49ba6066904dc583842541ba4697ded4c238da9d
abhiSolankii <abhisheksolanki01010@gmail.com>
2023-11-07 17:46:51 +0530

    Updates

---
+++ buycart.js
@@ -0,0 +1,8 @@
+<script>
+  // Function to automatically move the carousel function autoMoveCarousel(){" "}
+  {
+    $("#carouselExampleIndicators").carousel("next") // Move to the next slide
+  }
+  // Set an interval to call the function every 2 seconds (2000 milliseconds)
+  setInterval(autoMoveCarousel, 2000);
+</script>;
